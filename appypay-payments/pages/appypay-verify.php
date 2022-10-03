faeade31fd981feeab6d5f08de80d23433cc69b2
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2022-10-03 22:39:22 +0200

    - Removing useless methods  - Creating resources on the fly  - Fixing empty descrition and title  - Adding dummy redirect to

---
+++ appypay-payments/pages/appypay-verify.php
@@ -0,0 +1,3 @@
+<?php
+  echo '<h1>Verifying</h1>';
+?>
--- appypay-payments/payment_gateway.php
+++ appypay-payments/payment_gateway.php
@@ -18,7 +18,9 @@ Class Payment_Gateway extends WC_Payment_Gateway
 		$this->method_description = "Pagamento Online com Multicaixa Express";
 		$this->init_form_fields();
 		$this->init_settings();
-		$this->redirectTo = $this->get_option('redirect_to');
+		$this->title = "Multicaixa Express";
+		$this->description = "Pagamento Online com Multicaixa Express";
+		$this->redirectTo =  get_site_url() . '/appypay-verify.php'; //$this->get_option('redirect_to');
 		$this->application_id = 'GPO_5D7A07BC-24EB-4CC7-8218-109E61C62093'; //$this->get_option('application_id');
 		$this->testmode	= $this->get_option( 'testmode' );
 		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
@@ -31,14 +33,17 @@ Class Payment_Gateway extends WC_Payment_Gateway
 
 	public function process_payment($orderId)
 	{
-		$this->add_checkout_page();
+		// Creating basic resources at WP dir.
+		$this->move_checkout_file_to_themes_dir();
+		$this->move_verify_file_to_themes_dir();
+
 		$order = new WC_Order( $orderId );
 		// Appypay widgets variables
 		$amoumt = $this->get_order_total();
 		$description = "Order id ".$orderId;
 		$referenceNumber = $orderId;
 		$paymentMethod = $this->application_id;
-		$lang = 'pt';
+		$lang = 'pt-PT';
 		$redirectURI = $this->redirectTo;
 
 		$product_list = "";
@@ -50,7 +55,7 @@ Class Payment_Gateway extends WC_Payment_Gateway
 			'referenceNumber' => $referenceNumber,
 			'paymentMethod' => $paymentMethod,
 			'lang' => $lang,
-			'redirectURI' => $redirectTo
+			'redirectURI' => $redirectURI
 		);
 
 		return array(
@@ -60,50 +65,35 @@ Class Payment_Gateway extends WC_Payment_Gateway
 
 	}
 
-	public static function log( $message, $level = 'info' ) {
-		if ( self::$log_enabled ) {
-			if ( empty( self::$log ) ) {self::$log = wc_get_logger(); }
-			self::$log->log( $level, $message, array( 'source' => 'Appypay Pagamentos' ) );
-		}
-	}
-
-	// This is not working properly.
-	public function add_checkout_page()
-	{
-		$page_title = 'appypay-checkout';
-		$checkout_page = get_page_by_title($page_title, 'OBJECT', 'page');
-
-		if (empty($checkout_page)) {
-			wp_insert_post(
-				array(
-					'comment_status' => 'close',
-					'post_author'    => 1,
-					'post_title'     => $page_title,
-					'post_name'      => strtolower(str_replace(' ', '-', trim($page_title))),
-					'post_status'    => 'publish',
-					'post_content'   => '',
-					'post_type'      => 'page',
-					'page_template'  => 'appypay-checkout.php'
-				)
-			);
-			$this->move_checkout_file_to_themes_dir();
-		}
-	}
-
 	public function move_checkout_file_to_themes_dir()
 	{
 		$checkout_file_path = __DIR__  . "/pages/appypay-checkout.php";
-		$current_themes_path = get_template_directory();
-
+		$destination_file =  ABSPATH . "/appypay-checkout.php";
+		
 		$filesystem = new WP_Filesystem_Direct(false);
 
-		if ($filesystem->exists($checkout_file_path)) {
-			if ($filesystem->copy($checkout_file_path, $current_themes_path . "/appypay-checkout.php", true)) {
-			} else {
-					error_log("failed to copy file from " . $checkout_file_path . " to " . $current_themes_path);
+		if ($filesystem->exists($checkout_file_path) && !$filesystem->exists($destination_file)) {
+			if (!$filesystem->copy($checkout_file_path, ABSPATH. "/appypay-checkout.php", true)) {
+				error_log("failed to copy file from " . $checkout_file_path . " to " . $current_themes_path);
 			}
 		} else {
 			error_log("appypay-checkout.php was not found in plugin directory");
+		}
+	}
+
+	public function move_verify_file_to_themes_dir()
+	{
+		$verify_file_path = __DIR__  . "/pages/appypay-verify.php";
+		$destination_file = ABSPATH . "appypay-verify.php";
+
+		$filesystem = new WP_Filesystem_Direct(false);
+
+		if ($filesystem->exists($verify_file_path) && !$filesystem->exists($destination_file)) {
+			if (!$filesystem->copy($verify_file_path, ABSPATH. "/appypay-verify.php", true)) {
+				error_log("failed to copy file from " . $verify_file_path . " to " . $current_themes_path);
+			}
+		} else {
+			error_log("appypay-verify.php was not found in plugin directory");
 		}
 	}
 
